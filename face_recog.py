dc6b393016e6da686ce55ec2d15918e821aafbdc
Priya <priyamaheshwari183@gmail.com>
2023-01-29 20:39:35 +0530

    Update face_recog.py

--- face_recog.py
+++ face_recog.py
@@ -6,3 +6,24 @@ import os
 from datetime import datetime
 
 video_capture = cv2.VideoCapture(0)
+
+akshay_image = face_recognition.load_image_file("Photos/Akshay_kumar.jpg")
+akshay_encoding = face_recognition.face_encodings(akshay_image)[0]
+
+modi_image = face_recognition.load_image_file("Photos/Narendra_modi.jpg")
+modi_encoding = face_recognition.face_encodings(modi_image)[0]
+
+priyanka_image = face_recognition.load_image_file("Photos/Priyanka_chopra.jpg")
+priyanka_encoding = face_recognition.face_encodings(priyanka_image)[0]
+
+tata_image = face_recognition.load_image_file("Photos/Ratan_tata.jpg")
+tata_encoding = face_recognition.face_encodings(tata_image)[0]
+
+kohli_image = face_recognition.load_image_file("Photos/Virat_kohli.jpg")
+kohli_encoding = face_recognition.face_encodings(kohli_image)[0]
+
+known_face_encoding = [ akshay_encoding, modi_encoding, priyanka_encoding, tata_encoding, kohli_encoding]
+
+known_face_names = ["akshay", "modi", "priyanka", "tata", "kohli"]
+
+students = known_face_names.copy()
